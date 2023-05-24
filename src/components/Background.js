330f4df2a2e90c13398300ed3f752398037f9f10
WadeeKT <vector.sanna@gmail.com>
2023-05-24 13:23:45 +0200

    Finishing Landing Page.

---
+++ src/components/Background.js
@@ -0,0 +1,10 @@
+import React from "react";
+import background from "../utilities/videos/BackgroundHome.mp4";
+
+function Background() {
+    return(
+        <video autoPlay muted loop id="VideoBackground">
+            <source src={background} type="video/mp4" />
+        </video>
+    );
+};
