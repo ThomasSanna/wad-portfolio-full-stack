e18390933e9337847de5a9ae13baec3a364ebdfa
WadeeKT <thomassanna1e@gmail.com>
2023-05-25 01:05:29 +0200

    Contact + About Pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,15 +1,109 @@
 import Header from "../components/Header.js";
 import React from "react";
-import '../styles/About.css'
-import '../styles/reset.css'
+import "../styles/About.css";
+import "../styles/reset.css";
+import logowad from "../utilities/imgs/logowad.png";
+import { NavLink } from "react-router-dom";
 
 function About() {
   return (
     <div>
       <Header />
       <div className="page-container">
-        <h1>About</h1>
-        <p>This is the about page.</p>
+        <h1>About Me</h1>
+        <span className="logo-page-cont">
+          <img className="logo-page-wad" src={logowad} alt="Logo WadeeKT" />
+        </span>
+        <div className="about-container">
+          <section className="presentation-section sections">
+            <div className="div-title-section">
+              <h2 className="title-section">Presentation</h2>{" "}
+            </div>
+            <div className="bio">
+              <h3>SANNA (WadeeKT) Thomas</h3>
+              <p>
+                I am a passionate front-end web developer with nearly a year of
+                experience in the field. My goal is to create exceptional online
+                experiences using the latest technologies.
+              </p>
+              <p>
+                I have expertise in programming languages such as HTML,
+                JavaScript, CSS, as well as frameworks like React, Vue.js, and
+                Sass. I also have skills in PHP, MySQL, and Python. I have even
+                explored the world of Lisp.
+              </p>
+              <p>
+                As a freelance developer, I pay great attention to my clients'
+                expectations. I listen to their needs, ensure a clear
+                understanding of their objectives, and work closely with them to
+                deliver customized solutions that meet their requirements.
+              </p>
+              <p>
+                One of my strengths lies in my mastery of color theory, allowing
+                me to create visually appealing and harmonious designs. You can
+                explore some of my work on my{" "}
+                <NavLink className="liens" to="/portfolio">
+                  Portfolio page
+                </NavLink>
+                .
+              </p>
+            </div>
+          </section>
+        </div>
+        <NavLink to="/contact">
+          <section className="contact-section sections">
+            <div className="div-title-section">
+              <h2 className="title-section">Contact</h2>
+            </div>
+
+            <div className="contact">
+              <div className="contact-info">
+                <p>
+                  <span className="bold">Email :</span>{" "}
+                  <a className="liens" href="mailto:thomassanna1e@gmail.com">
+                    thomassanna1e@gmail.com
+                  </a>
+                </p>
+                <p>
+                  <span className="bold">Phone :</span>{" "}
+                  <a className="liens" href="tel:+336 67 38 13 35">
+                    +33 6 67 38 13 35
+                  </a>
+                </p>
+                <p>
+                  <span className="bold">Location :</span> Ajaccio, Corsica,
+                  France
+                </p>
+                <p className="liens-cont">
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    className="bold liens"
+                    href="https://github.com/wadeekt"
+                  >
+                    GitHub
+                  </a>
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    className="bold liens"
+                    href="https://www.linkedin.com/in/thomas-sanna-b74600277/"
+                  >
+                    Linkedin
+                  </a>
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    className="bold liens"
+                    href="https://twitter.com/WadeeKT"
+                  >
+                    Twitter
+                  </a>
+                </p>
+              </div>
+            </div>
+          </section>
+        </NavLink>
       </div>
     </div>
   );
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,18 +1,74 @@
 import React from "react";
 import Header from "../components/Header.js";
-import '../styles/Contact.css'
-import '../styles/reset.css'
+import "../styles/Contact.css";
+import "../styles/reset.css";
+import logowad from "../utilities/imgs/logowad.png";
+import linkedin from "../utilities/imgs/linkedin-svgrepo-com.svg";
+import github from "../utilities/imgs/github-142-svgrepo-com.svg";
+import twitter from "../utilities/imgs/twitter-svgrepo-com.svg";
 
 function Contact() {
-    return (
-        <div>
-            <Header />
-            <div className="page-container">
-                <h1>Contact</h1>
-                <p>This is the contact page.</p>
+  return (
+    <div>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        integrity="sha384-e2NqBzzw5b9caQ7e3nKm1RkCdFT+KhO7YMyt+JhzA1aB1g9/AK3OW0h4Sx9w9BrB"
+        crossorigin="anonymous"
+      />
+      <Header />
+      <div className="page-container">
+        <h1>Contact Me</h1>
+        <span className="logo-page-cont">
+          <img className="logo-page-wad" src={logowad} alt="Logo WadeeKT" />
+        </span>
+        <div className="sections">
+            <div className="i-reseaux-cont">
+              <a
+                title="My GitHub !"
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/WadeeKT"
+              >
+                <img className="i-reseaux i-github" src={github} alt="github profil" />
+              </a>
+              <a
+                title="My Twitter !"
+                target="_blank"
+                rel="noreferrer"
+                href="https://twitter.com/WadeeKT"
+              >
+                <img
+                  className="i-reseaux i-twitter"
+                  src={twitter}
+                  alt="twitter profil"
+                />
+              </a>
+              <a
+                title="My Linkedin !"
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.linkedin.com/in/thomas-sanna-b74600277/"
+              >
+                <img
+                  className="i-reseaux i-linkedin"
+                  src={linkedin}
+                  alt="linkedin profil"
+                />
+              </a>
+            </div>
+            <div className="form-cont">
+                <form className="form">
+                    <input type="text" id="name" placeholder="Your name" />
+                    <input type="email" id="email" placeholder="Your email" />
+                    <textarea id="message" placeholder="Your message"></textarea>
+                    <button type="submit">Send</button>
+                </form>
             </div>
         </div>
-    );  
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
